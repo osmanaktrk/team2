bb4187763580e8f2d78c8e1014d629a72efbbb45
MalikBogos <malikjandarov@gmail.com>
2024-05-08 11:43:34 +0200

    add a php with opleidingen & update sources for assets in the MainPagina.html page

---
+++ Opleidingen.php
@@ -0,0 +1,14 @@
+<?php 
+$graduaatopleiding = ["Accounting administration", "Accounting administration (e-learning)", "Basisverpleegkunde", "Educatief graduaat secundair onderwijs", "Elektromechanische systemen", "Informatiebeheer", "Internet of things", "Juridisch-administratieve ondersteuning (werktraject)", "Juridisch-administratieve ondersteuning", "Logies-, restaurant- & cateringmanagement", "Marketing- & communicatiesupport", "Programmeren", "Sociaal-cultureel werk", "Systeem- & netwerkbeheer", "Winkelmanagement"];
+
+
+$bacheloropleiding = ["Biomedische laboratoriumtechnologie", "Communicatie", "Hotelmanagement", "Idea and innovation management", "Journalistiek", "Kleuteronderwijs", "Lager onderwijs", "Landschaps-& tuinarchitectuur", "Multimedia & creatieve technologie", "Organisatie & management", "Pedagogie van het jonge kind", "Secundair onderwijs", "Sociaal werk", "Toegepaste informatica", "Toerisme -& recreatiemanagement", "Verkorte educatieve bachelor lager onderwijs", "Verkorte educatieve bachelor secundair onderwijs", "Verpleegkunde", "Voedings-& dieetkunde", "Vroedkunde"];
+
+$lerarenopleiding = ["Educatief graduaat secundair onderwijs", "Kleuteronderwijs", "Lager onderwijs", "Secundair onderwijs", "Verkorte educatieve bachelor lager onderwijs", "Verkorte educatieve bachelor secundair onderwijs"];
+
+$kunstopleiding = ["Academische bachelor/master audiovisuele kunsten", "Academische bachelor/master drama", "Academische bachelor/master muziek", "Educatieve opleidingen in de kunsten", "Podium- en eventtechnieken", "Professionele bachelor audiovisuele kunsten", "Professionele bachelor/master musical"];
+
+$postgraduaatopleiding = ["Applied artificial intelligence ('24-'25)", "Applied bio-informatics in research & diagnostics", "Bedrijfsverpleegkunde", "Coding (online)", "Cosmetic sciences", "Diabeteseducator", "Fiscaliteit ('24-'25)", "Forensisch hulpverlenend expert", "Forensisch onderzoek - volzet", "Forensisch verpleegkundig expert", "Health coach", "Informatiemanagement ('24-'25)", "Musical", "Muziek", "Niet-confessionele zedenleer", "Palliatieve zorg", "Pediatrie & neonatologie",  "Peri-operatieve zorgen", "Procesbegeleider ethiek in zorg en welzijn", "Technologisch verpleegkundige", "Toegepaste artificiële intelligentie"];
+
+$bachelor_na_bacheloropleiding = ["Zorgmanagement"];
+?>
