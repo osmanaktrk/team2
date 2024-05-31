e88c9ea631e5bfaa46dcf8b068ee01e7439f760c
NdizeyeArmand <armand-gael.ndizeye@student.ehb.be>
2024-05-31 17:55:56 +0200

    create PostController + add categories

---
+++ laravel-breeze/app/Http/Controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class PostController extends Controller
+{
+    //
+}
--- laravel-breeze/database/seeders/CategorySeeder.php
+++ laravel-breeze/database/seeders/CategorySeeder.php
@@ -14,7 +14,7 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     { 
-        $categories = collect(['School','Politics', 'Gaming', 'Movies & Series', 'Cooking']);
+        $categories = collect(['Education', 'General', 'Politics', 'Gaming', 'Movies & Series', 'Cooking']);
 
         $categories->each(fn ($category) => Category::create([
             'name' => $category,
