a76c0d00bc4c7320e38455efc9d3147ba3aa64cd
MalikBogos <malikjandarov@gmail.com>
2024-06-02 00:39:39 +0200

    Changed left-menu links

--- laravel-breeze/routes/web.php
+++ laravel-breeze/routes/web.php
@@ -32,11 +32,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/profile', function () {
+    return view('edit');
+})->middleware(['auth', 'verified'])->name('profile');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
 require __DIR__.'/student.php';
