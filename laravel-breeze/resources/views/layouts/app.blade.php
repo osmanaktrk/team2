7ffe69142054c5ddc5f1d9d9938e258e522df92f
NdizeyeArmand <armand-gael.ndizeye@student.ehb.be>
2024-06-03 15:12:45 +0200

    create logo in title

--- laravel-breeze/resources/views/layouts/app.blade.php
+++ laravel-breeze/resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - {{ config('app.name', 'EraStudent') }}</title>
-    <!-- <link rel="icon" type="image/png" href="{{ asset('img/ehb.png') }}" /> -->
+    <link rel="icon" type="image/png" href="{{ asset('img/ehb.png') }}" />
     @stack('head')
 </head>
 <body>
