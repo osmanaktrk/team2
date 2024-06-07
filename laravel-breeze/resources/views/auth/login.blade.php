273ec4f60d77e764eb364f98ca319946fbfd6ae2
NdizeyeArmand <armand-gael.ndizeye@student.ehb.be>
2024-06-07 21:55:22 +0200

    fix a buttons issue in login page

--- laravel-breeze/resources/views/auth/login.blade.php
+++ laravel-breeze/resources/views/auth/login.blade.php
@@ -65,18 +65,14 @@
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-
-            <x-primary-button class="ms-3 flex items-center gap-4">
-                {{ __('Log in') }}
-            </x-primary-button>
-            
         </div>
     </form>
     <br>
     <div class="flex items-center justify-center">
-        {{-- <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
-            {{ __('REGISTER') }} 
-        </a> --}}
+        <x-primary-button class="ms-3 flex items-center gap-4">
+            {{ __('Log in') }}
+        </x-primary-button>
+        <div style="width: 10px;"></div>
         <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" href="{{ route('register') }}">
             {{ __('REGISTER') }}
         </a>
